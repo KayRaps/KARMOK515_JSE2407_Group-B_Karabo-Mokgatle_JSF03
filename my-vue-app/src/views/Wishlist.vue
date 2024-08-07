d9ca9035e7f57c602a83ae3bd81e8b20444cbef2
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-08-07 13:21:54 +0200

    Created Wishlist.vue component with basic template structure.

---
+++ my-vue-app/src/views/Wishlist.vue
@@ -0,0 +1,25 @@
+<!-- Wishlist.vue -->
+<template>
+    <div>
+      <h1>Your Wishlist</h1>
+      <!-- Wishlist content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Wishlist',
+    data() {
+      return {
+        items: []
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
