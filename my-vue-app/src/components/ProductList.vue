9a98f9e0a378137d3e0c70e53a7476e4860aa51f
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-08-08 15:58:51 +0200

    fixed typo

--- my-vue-app/src/components/ProductList.vue
+++ my-vue-app/src/components/ProductList.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="container mx-auto p-4">
       <!-- Header -->
-      <h1 class="text-2xl font-bold mb-4 text-center">Vue E-store</h1>
+      <h1 class="text-2xl font-bold mb-4 text-center">Welcome to Ecommerce-store</h1>
 
       <!-- Filters Section -->
       <div class="filters">
